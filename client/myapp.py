1b603cf623be571392dd07d5e8897f895caf4900
alie <mushyafaali@gmail.com>
2023-12-22 00:06:13 +0700

    membuat halaman login dan halaman products

--- client/myapp.py
+++ client/myapp.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/login")
+def login():
+    return render_template('/login.html')
+
 @app.route("/")
 @app.route("/index")
 def index():
