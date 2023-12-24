aba1657421b91b4eae1600e30e3e4e50ebe3266f
alie <mushyafaali@gmail.com>
2023-12-24 14:39:44 +0700

    added a library namely flask_cors

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
+
+CORS(app)
 app.config.from_object(Config)
 
 jwt = JWTManager(app)
